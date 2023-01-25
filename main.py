f57a130eae3dc3edd2bb83de7d6da58db1d41ef5
sudharaD <sudharad7@gmail.com>
2023-01-25 23:20:59 +0530

    snake game - started

---
+++ main.py
@@ -0,0 +1,16 @@
+# create a snake body
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+
+screen.screensize(400, 400)
+
+screen.exitonclick()
+# move the snake
+# control the snake
+# detect collision with food
+# create a scoreboard
+# detect collision with wall
+# detect collision with tail
+
