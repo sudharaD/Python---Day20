89d3f453c9195dcff6c2637559c812c35fdfc76b
sudharaD <sudharad7@gmail.com>
2023-01-26 21:55:55 +0530

    snake game - snake created with snake class

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-# create a snake body
-# move the snake
-# control the snake
-# detect collision with food
-# create a scoreboard
-# detect collision with wall
-# detect collision with tail
-from turtle import Turtle, Screen
-
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My snake game")
-
-turtle1 = Turtle()
-turtle1.penup()
-turtle1.shape("square")
-turtle1.color("white")
-turtle2 = Turtle()
-turtle2.penup()
-turtle2.shape("square")
-turtle2.color("white")
-turtle2.goto(-20, 0)
-turtle3 = Turtle()
-turtle3.penup()
-turtle3.shape("square")
-turtle3.color("white")
-turtle3.goto(-40, 0)
-print(turtle2.pos())
-
+# TODO: create a snake body
 # screen.register_shape("snake", ((10, -50), (10, 10), (-10, 10), (-10, -50)))
 # turtle.shape("snake")
+from snake import Snake
 
-
-screen.exitonclick()
+# TODO: move the snake
+# TODO: control the snake
+# TODO: detect collision with food
+# TODO: create a scoreboard
+# TODO: detect collision with wall
+# TODO: detect collision with tail
 
 
--- snake.py
+++ snake.py
@@ -1,8 +1,21 @@
 class Snake:
     from turtle import Turtle, Screen
 
-    turtle = Turtle()
     screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My snake game")
 
-    def __int__(self):
+    POSITION_LIST = [(0, 0), (-20, 0), (-40, 0)]
+
+    for position in POSITION_LIST:
+        turtle = Turtle()
+        turtle.penup()
+        turtle.goto(position)
+        turtle.shape("square")
+        turtle.color("white")
+
+    def __init__(self):
         pass
+
+    screen.exitonclick()
